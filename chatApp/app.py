8895efa0469c3c32315ecdff7534325c3072b6a6
nibankar <nikhil.10641042@ltimindtree.com>
2023-10-13 02:01:08 +0000

    00 auto save by knights-watch

--- chatApp/app.py
+++ chatApp/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import torch
 from transformers import pipeline
-
+import time
 #pipe = pipeline("text-generation", model="/data/huggingface/cache/models--HuggingFaceH4--starchat-beta/snapshots/b1bcda690655777373f57ea6614eb095ec2c886f", torch_dtype=torch.bfloat16, device_map="auto")
 
 
