16f4622e38d2973efb415f08421fe0af3d80769b
nibankar <nikhil.10641042@ltimindtree.com>
2023-10-16 06:39:32 +0000

    00 auto save by knights-watch

--- gradioContext1/app.py
+++ gradioContext1/app.py
@@ -3,6 +3,15 @@
 def echo(message, history):
     return message
 
+
+import torch
+from transformers import pipeline
+
+#pipe = pipeline("text-generation", model="HuggingFaceH4/starchat-beta", torch_dtype=torch.bfloat16, device_map="auto")
+pipe = pipeline("text-generation", model="/data/huggingface/cache/models--HuggingFaceH4--starchat-beta/snapshots/b1bcda690655777373f57ea6614eb095ec2c886f", torch_dtype=torch.bfloat16, device_map="auto")
+
+
+
 # demo = gr.ChatInterface(fn=echo, examples=["hello", "hola", "merhaba"], title="Echo Bot")
 
 #demo.queue().launch(root_path="/test")
